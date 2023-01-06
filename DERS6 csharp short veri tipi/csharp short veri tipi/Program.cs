fe70bc51dcfa03c8d5450f3361667f07131df2d1
Okan Çelebi <okancelebi06@gmail.com>
2023-01-06 13:18:11 +0300

    Add files via upload

---
+++ DERS6 csharp short veri tipi/csharp short veri tipi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_short_veri_tipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte birinci = 5;
+            Byte ikinci = 25;
+            Byte ucuncu = 255;
+            Byte MınDeger = Byte.MaxValue;
+            Byte MaxDeger = Byte.MinValue;
+            //Byte 0-255 arasındaki tam sayıları kabul eder.
+            //cw ve 2 kez tab basınca: "Console.WritLine" yazar.
+            Console.WriteLine(birinci);
+            Console.WriteLine(ikinci);
+            Console.WriteLine(ucuncu);
+            Console.WriteLine(MınDeger);
+            Console.WriteLine(MaxDeger);
+            Console.ReadLine();
+        }
+    }
+}
